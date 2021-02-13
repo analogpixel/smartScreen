9f2e092eeaaaf97cba82adb26f4bd7b00d152116
analogpixel <matt.poepping@gmail.com>
2021-02-13 14:15:46 -0700

    slideshow

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-from flask import Flask
+from flask import Flask,send_file
 import socket
 import struct
+import glob
+import random
 
 app = Flask(__name__)
 
@@ -11,6 +13,15 @@ def send(m):
     client.send(m.encode('utf8'))
     from_server = client.recv(4096)
     client.close()
+
+@app.route('/slideshow/')
+def slideshow_show():
+    return app.send_static_file('slideshow/index.html')
+
+@app.route('/slideshow/random_img/')
+def slideshow_random_img():
+    img = random.choice( list(filter( lambda x: x.split('.')[-1].lower() in ['png','jpg','gif'] , glob.glob("static/content/insp_pictures/**/*", recursive=True) ) ))
+    return send_file(img, mimetype='image/' + img.split('.')[-1].lower() )
 
 @app.route('/url/<prefix>/<postfix>')
 def open_url(prefix,postfix):
